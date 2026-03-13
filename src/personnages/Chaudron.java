2c4d3fd880cb24520924e4734d6d4e368754f995
Gael Mialon <mlg5759a@ensfsi.onmicrosoft.com>
2026-03-13 09:40:31 +0100

    methodes Chaudron Druide et Gaulois

--- src/personnages/Chaudron.java
+++ src/personnages/Chaudron.java
@@ -10,14 +10,23 @@ public class Chaudron {
 	}
 
 	public void remplirChaudron(int quantite, int forcePotion) {
-
+		quantitePotion = quantite;
+		this.forcePotion = forcePotion;
 	}
 
-	public void resterPotion() {
-
+	public boolean resterPotion() {
+		if (quantitePotion == 0) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 
-	public void prendreLouche() {
-
+	public int prendreLouche() {
+		quantitePotion -= 1;
+		if (quantitePotion == 0) {
+			forcePotion = 0;
+		}
+		return forcePotion;
 	}
 }
--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -3,26 +3,40 @@ package personnages;
 public class Druide {
 	private String nom;
 	private int force;
+	private Chaudron chaudron;
 
-	private Druide(String nom, int force) {
+	public Druide(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
 	}
 
 	public void parler(String texte) {
-
+		System.out.println(prendreParole() + "\"" + texte + "\"");
 	}
 
-	private static void prendreParole() {
-
+	private String prendreParole() {
+		return "Le Druide " + nom + " : ";
 	}
 
 	public void fabriquerPotion(int quantite, int forcePotion) {
-
+		chaudron.remplirChaudron(quantite, forcePotion);
+		parler("J'ai concocté " + quantite + " doses de potoin magique. Elle a une force de " + forcePotion + ".");
 	}
 
 	public void booster(Gaulois gaulois) {
-
+		boolean contientPotion = chaudron.resterPotion();
+		String nomgaulois = gaulois.getNom();
+		if (contientPotion) {
+			if (nomgaulois == "Obélix") {
+				parler("Non, " + nomgaulois + " Non !... Et tu le sais très bien !");
+			} else {
+				int forcePotion = chaudron.prendreLouche();
+				gaulois.boirePotion(forcePotion);
+				parler("Tiens " + nomgaulois + " un peu de potion magique.");
+			}
+		} else {
+			parler("Désolé " + nomgaulois + " il n'y a plus une seule goutte de potion.");
+		}
 	}
 
 	public String getNom() {
